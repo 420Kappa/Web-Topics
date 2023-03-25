78b100e05046058c4bfdc5a4fb7855a54c189353
Dries <dries.dekoker@student.odisee.be>
2023-03-25 17:59:02 +0100

    Start open source

---
+++ src/js/background-gradient.ts
@@ -0,0 +1,62 @@
+import { Shader } from 'three';
+
+interface ShaderData {
+  [key: string]: any;
+}
+
+interface GenericShader<T extends ShaderData> extends Shader {
+  schema: {
+    [P in keyof T]: {
+      type: string;
+      default: any;
+      is?: string;
+    }
+  };
+}
+
+interface BackgroundGradientShaderData extends ShaderData {
+  colorTop: string;
+  colorBottom: string;
+}
+
+const BackgroundGradientShader: GenericShader<BackgroundGradientShaderData> = {
+  schema: {
+    colorTop: { type: 'color', default: '#37383c', is: 'uniform' },
+    colorBottom: { type: 'color', default: '#757575', is: 'uniform' }
+  },
+
+  vertexShader: [
+    'varying vec3 vWorldPosition;',
+
+    'void main() {',
+
+    'vec4 worldPosition = modelMatrix * vec4( position, 1.0 );',
+    'vWorldPosition = worldPosition.xyz;',
+
+    'gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );',
+
+    '}'
+  ].join('\n'),
+
+  fragmentShader: [
+    'uniform vec3 colorTop;',
+    'uniform vec3 colorBottom;',
+
+    'varying vec3 vWorldPosition;',
+
+    'void main()',
+
+    '{',
+    'vec3 pointOnSphere = normalize(vWorldPosition.xyz);',
+    'float f = 1.0;',
+
+    'f = sin(pointOnSphere.y * 2.2) + 0.4;',
+
+    'gl_FragColor = vec4(mix(colorBottom, colorTop, f), 1.0);',
+
+    '}'
+  ].join('\n'),
+  uniforms: {}
+};
+
+AFRAME.registerShader('background-gradient', BackgroundGradientShader);
